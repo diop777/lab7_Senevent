682fb999a8df9ab704596be376fa8caf93e1529c
Ousseynou Diop <ousseynoudiopcoki@gmail.com>
2026-07-17 11:36:43 +0100

    auth Supabase : signup, login, deconnexion, NavBar contextuelle

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { supabase } from "./lib/supabase";
 import Accueil from "./pages/Accueil";
 import NouvelEvenement from "./pages/NouvelEvenement";
 import Detail from "./pages/Detail";
+import Auth from "./pages/Auth";
 import NavBar from "./components/NavBar";
 
 const App = () => {
   const [evenements, setEvenements] = useState([]);
   const [chargement, setChargement] = useState(true);
   const [erreur, setErreur] = useState(null);
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+    });
+
+    const { data: subscription } = supabase.auth.onAuthStateChange(
+      (_event, newSession) => {
+        setSession(newSession);
+      }
+    );
+
+    return () => subscription.subscription.unsubscribe();
+  }, []);
 
   const charger = async () => {
     setChargement(true);
@@ -35,7 +52,7 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar session={session} />
       <Routes>
         <Route
           path="/"
@@ -56,6 +73,7 @@ const App = () => {
           path="/evenement/:id"
           element={<Detail evenements={evenements} />}
         />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
